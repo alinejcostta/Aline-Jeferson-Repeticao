b35e7e2c86f16417cf64323483f2a762f0dbbfbf
alinejcostta <279850155+alinejcostta@users.noreply.github.com>
2026-05-04 21:32:57 -0300

    .

--- exercicio23.py
+++ exercicio23.py
@@ -4,13 +4,14 @@
 # 23. Faça um programa que mostre todos os primos entre 1 e N sendo N um número inteiro fornecido pelo usuário. O programa deverá mostrar também o número de divisões que ele executou para encontrar os números primos. Serão avaliados o funcionamento, o estilo e o número de testes (divisões) executados.
 
 numero = int(input("Digite um número inteiro: "))
-
+operacoes = 0
 
 for n in range (1, numero+1):
     
     primo = True
 
-    for i in range (2, n):
+    for i in range (2, (n//2)+1):
+        operacoes +=1
         if n % i == 0:
             primo = False
             break
@@ -20,3 +21,4 @@ for n in range (1, numero+1):
     else:
         print(f"{n} não é primo!")
         
+print(f"Foram usadas {operacoes} operações")
